13a0d4b223867eaaf7ac0dc268dae03f138bd2e2
Oleg Yakonen <hronos1228@yandex.ru>
2022-11-02 16:55:34 +0300

    первый коммит

---
+++ Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Povarenok
+{
+    partial class FormShablon
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormShablon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 408);
+            this.Name = "FormShablon";
+            this.Text = "Шаблон";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
